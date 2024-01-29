6087ec24ca08161aa6518a46dcc6567c1ae4a130
lethalgem <iwc123@gmail.com>
2024-01-28 21:48:14 -0600

    add dynamic crosshair

---
+++ controllers/reticle.gd
@@ -0,0 +1,18 @@
+extends CenterContainer
+
+@export var DOT_RADIUS: float = 1.0
+@export var DOT_COLOR: Color = Color.WHITE
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass  # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _draw():
+	draw_circle(Vector2(0, 0), DOT_RADIUS, DOT_COLOR)
